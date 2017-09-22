2cc81b0960ae8d2ea8c1cd5ec299aad3749244cb
Amalina Kurniasari <amalinakurniasari@gmail.com>
2017-09-22 10:53:15 +0700

    Web Developer Test

---
+++ app/Books.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Books extends Model
+{
+    public function user() {
+		return $this->belongsTo(User::class);
+	}
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username', 100)->unique();
+            $table->string('fullname');
+            $table->string('password');
+            $table->string('salt');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+            $table->timestamp('deleted_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
